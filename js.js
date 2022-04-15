7133179c3ef86fc58b47abd2cf4866113ac58a18
myielin <mfernandamf114@gmail.com>
2022-04-15 19:59:15 -0300

    sobre

--- js.js
+++ js.js
@@ -1,8 +1,11 @@
 console.log("hi!")
+
 $("#sobre").click(function(){
 $("#conteudo").load("sobre.html")
 })
-
+$("#galeria").click(function(){
+  $("#conteudo").load("galeria.html #conteudo");
+})
 
 
 //document.addEventListener("scroll", document.getElementById('links').style.position = "fixed")
