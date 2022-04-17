1c9937fafead5093712f78b505ca03e9926ff66d
myielin <mfernandamf114@gmail.com>
2022-04-17 13:59:12 -0300

    design

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 console.log("hi!")
 
-
+$("#logo").click(function(){
+  window.location.href="index.html"
+})
 
 //document.addEventListener("scroll", document.getElementById('links').style.position = "fixed")
